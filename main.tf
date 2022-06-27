c72e2e92c4b169273803774070b276006b8d9d2e
ordijdc <102463712+ordijdc@users.noreply.github.com>
2022-06-27 19:36:19 +0200

    feat: Ficheros iniciales de Terraform

---
+++ main.tf
@@ -0,0 +1,15 @@
+/*   Main execution file, usually it has providers configuration */
+
+provider "aws" {
+  region  = "eu-west-1"
+  //profile = "default"  
+}
+
+resource "aws_s3_bucket" "reto" {
+  bucket = "mapfre-gitops-ordijdc"
+}
+
+resource "aws_s3_bucket_acl" "reto_bucket_acl" {
+  bucket = aws_s3_bucket.reto.id
+  acl    = "public-read"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+// this file is always included in terraform runs
+// you can also use include .tfvars in commandline, export TF_VAR_image_id=ami-abc123 etc
+// -var-file="testing.tfvars"
+
+
